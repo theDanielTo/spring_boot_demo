88eb8cf558ef7e00f42a4abc1515a491ba46bc32
theDanielTo <dto1989@gmail.com>
2022-01-14 16:26:19 -0800

    Return list of POJO as JSON

--- src/main/java/com/danielto/demo/web/UserController.java
+++ src/main/java/com/danielto/demo/web/UserController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import com.danielto.demo.Product;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,5 +31,13 @@ public class UserController {
   @RequestMapping("/{userId}/items")
   public List<String> displayStringJson() {
     return Arrays.asList("Shoes", "laptop", "button");
+  }
+
+  @RequestMapping("/{userId}/products_as_json")
+  public List<Product> displayProductsJson() {
+    return Arrays.asList(
+      new Product(1, "shoes", 42.99),
+      new Product(2, "books", 12.99),
+      new Product(3, "bag", 45.99));
   }
 }
