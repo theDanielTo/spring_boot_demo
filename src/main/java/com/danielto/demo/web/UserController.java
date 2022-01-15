a2360e02976d28cdda2147ecd0bf5e15df3452da
theDanielTo <dto1989@gmail.com>
2022-01-14 16:17:56 -0800

    Returning List<String> as JSON

--- src/main/java/com/danielto/demo/web/UserController.java
+++ src/main/java/com/danielto/demo/web/UserController.java
@@ -1,6 +1,8 @@
 package com.danielto.demo.web;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,16 @@ public class UserController {
     return "User Found: " + userId;
   }
 
-  @RequestMapping("/{userId}/invoices")
-  public String displayUserInvoices(@PathVariable int userId, @RequestParam Date date) {
-    return "Invoice found for user: " + userId + " on the date: " + date;
+  @RequestMapping("/{id}/invoices")
+  public String displayUserInvoices(
+    @PathVariable("id") int userId,
+    @RequestParam(value="date", required=false) Date dateOrNull
+    ) { // localhost:8080/user/32/invoices?date=01/14/2022
+    return "Invoice found for user: " + userId + " on the date: " + dateOrNull;
+  }
+
+  @RequestMapping("/{userId}/items")
+  public List<String> displayStringJson() {
+    return Arrays.asList("Shoes", "laptop", "button");
   }
 }
