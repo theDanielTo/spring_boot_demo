b861a77118fa7f657f85efcfff9697d579e02cca
theDanielTo <dto1989@gmail.com>
2022-01-16 11:00:16 -0800

    HomeController with User class

---
+++ src/main/java/com/danielto/demo/web/HomeController.java
@@ -0,0 +1,19 @@
+package com.danielto.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+  @RequestMapping("/")
+  public String home() {
+    return "index";
+  }
+
+  @RequestMapping(value="/create", method=RequestMethod.POST)
+  public String processFormData() {
+    return "";
+  }
+}
